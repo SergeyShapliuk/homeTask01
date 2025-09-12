7dee32dd993bdcf7b091d5ff5f1811a2a09840bf
shapl_xlbobtq <sergeshapluk@gmail.com>
2025-09-12 13:57:29 +0300

    add swagger

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,17 +1,20 @@
-import express, { Express } from 'express';
-import { videoRouter } from './videos/routers/video.router';
-import { testingRouter } from './testing/routers/testing.router';
-import { HttpStatus } from './core/types/http-ststuses';
+import express, {Express} from "express";
+import {videoRouter} from "./videos/routers/video.router";
+import {testingRouter} from "./testing/routers/testing.router";
+import {HttpStatus} from "./core/types/http-ststuses";
+import {setupSwagger} from "./core/swagger/setup-swagger";
 
 export const setupApp = (app: Express) => {
-  app.use(express.json()); // middleware для парсинга JSON в теле запроса
+    app.use(express.json()); // middleware для парсинга JSON в теле запроса
 
-  app.use('/videos', videoRouter); // Подключаем роутеры
-  app.use('/testing', testingRouter); // Подключаем тестовый роутер
+    app.use("/videos", videoRouter); // Подключаем роутеры
+    app.use("/testing", testingRouter); // Подключаем тестовый роутер
 
     app.get("/", (req, res) => {
         res.status(HttpStatus.Ok).send("Hello world!");
     });
 
-  return app;
+    setupSwagger(app);
+
+    return app;
 };
