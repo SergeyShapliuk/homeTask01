75a529e4236ddd63d85b75fe231a80f5b66e57af
shapl_xlbobtq <sergeshapluk@gmail.com>
2025-10-29 13:08:43 +0300

    commit

--- src/core/adapters/nodemailer.service.ts
+++ src/core/adapters/nodemailer.service.ts
@@ -15,12 +15,12 @@ export const nodemailerService = {
         let transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                type: "OAuth2",
+                // type: "OAuth2",
                 user: process.env.GMAIL_USER,
-                // pass: process.env.PASS_SMTP
-                clientId: process.env.GMAIL_CLIENT_ID,
-                clientSecret: process.env.GMAIL_CLIENT_SECRET,
-                refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+                pass: process.env.PASS_SMTP
+                // clientId: process.env.GMAIL_CLIENT_ID,
+                // clientSecret: process.env.GMAIL_CLIENT_SECRET,
+                // refreshToken: process.env.GMAIL_REFRESH_TOKEN,
                 // accessToken: process.env.GMAIL_ACCESS_TOKEN
             }
             // tls: {
