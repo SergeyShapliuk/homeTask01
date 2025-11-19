cfc9add216489bda8564bdcffb740b951752da7c
shapl_xlbobtq <sergeshapluk@gmail.com>
2025-11-19 15:08:42 +0300

    commit

--- src/coments/routers/handlers/update-like-status.handler.ts
+++ src/coments/routers/handlers/update-like-status.handler.ts
@@ -19,16 +19,16 @@ export async function updateLikeStatusHandler(
         const likeStatus = req.body.likeStatus;
         console.log("updateLikeStatusHandler", {userId});
         console.log("updateLikeStatusHandler", {commentId});
-        // const comment = await commentRepository.findByIdOrFail(commentId);
-        // if (comment.commentatorInfo.userId !== userId) {
-        //     res.status(HttpStatus.Forbidden).send("If try delete the comment that is not your own");
-        //     return;
-        // }
         if (!userId) {
             res.sendStatus(HttpStatus.Unauthorized);
             return;
         }
-
+        const comment = await commentRepository.findByIdOrFail(commentId);
+        if (comment.commentatorInfo.userId !== userId) {
+            res.status(HttpStatus.Forbidden).send("If try delete the comment that is not your own");
+            return;
+        }
+        
         console.log("updateCommentHandler", commentId);
         await commentLikeService.updateLikeStatus(commentId, userId, likeStatus);
         res.sendStatus(HttpStatus.NoContent);
